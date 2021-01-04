097c9dff634c0652232337b441537286ff5b2b2f
Aleksei Miro <alex.miro20@gmail.com>
2021-01-05 01:19:59 +0200

    add ifs and elses. Train branching and looping

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 "use strict";
 
+
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,13 +12,26 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < numberOfFilms ; i++) {
+for (let i = 0; i < numberOfFilms; i++) {
     const oneOfFilms = prompt("Один из последних фильмов?", "");
     const grade = prompt("На сколько оцените его?", "");
-
-    personalMovieDB.movies[oneOfFilms] = grade;
+    if (oneOfFilms != "" && oneOfFilms != null && grade != "" && grade != null && oneOfFilms < 50) {
+        personalMovieDB.movies[oneOfFilms] = grade;
+    } else {
+        i--;
+        alert("ошибка ввода, повторите ввод");
+    }
 }
 
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
 
 
 console.log(personalMovieDB);
