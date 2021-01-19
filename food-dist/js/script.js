23dae44199a21d05acfd298050670dec33b5a0e1
Aleksei Miro <alex.miro20@gmail.com>
2021-01-19 14:29:07 +0200

    add timer to script in food-dist.js

--- food-dist/js/script.js
+++ food-dist/js/script.js
@@ -48,14 +48,56 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
         }
-    })
+    });
+
+    // Timer
+
+    const deadline = '2021-01-20';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t / (60 * 60 * 24 * 1000)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = t.days >= 10 ? t.days : `0${t.days}`;
+            hours.innerHTML = t.hours >= 10 ? t.hours : `0${t.hours}`;
+            minutes.innerHTML = t.minutes >= 10 ? t.minutes : `0${t.minutes}`;
+            seconds.innerHTML = t.seconds >= 10 ? t.seconds : `0${t.seconds}`;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+
+    }
 
 
+    setClock('.timer', deadline);
 
 
-});
-
-
-array.forEach((element,i) => {
-    
 });
